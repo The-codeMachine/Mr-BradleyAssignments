ea45ea7af9bd122e3e495d77231030474e160752
Mr-Bradley <rbradley@edisonschool.ca>
2026-03-23 13:22:48 -0600

    Design & Documentation is required and must be updated if you change the design. See NB

--- Light/src/main/java/light/Light.java
+++ Light/src/main/java/light/Light.java
@@ -1,5 +1,27 @@
 package light;
 
+/**
+ * Concept / Use 
+ * =============
+ * A Light represents a the concept similar to a light in a house
+ * with an on/off button and a brighten / dim switch or dial.
+ * Pressing the on/off button turns the light on, after which
+ * pressing the brighten / dim switches increase and decrease the 
+ * luminosity of the light by 10%
+ * 
+ * Design
+ * ======
+ *  A light is encoded as an int value with a valid range between
+ *  [0..100]  0 - OFF, 1 - MIN, 100 - MAX Any values outside this range
+ *  are invalid.
+ *  
+ *  Operations include:
+ *  
+ *      o turning on / off the light
+ *      o brighten / dim the light by 10% NB: luminosity changes can only occur if the light is ON
+ *      o query the light for its state - on | off, and luminosity
+ */
+
 public class Light {
 
     // Core API
