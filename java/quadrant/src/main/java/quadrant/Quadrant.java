432b7953c664047268893d1c6cb319decd8ca0ac
Mr-Bradley <rbradley@edisonschool.ca>
2026-04-10 10:19:40 -0600

    Code Review
    It is not clear to me that you used the process we discussed for writing code / classes:
    
    Understand and document the problem
    Design and document it
    Establish the public API
    Establish the test suite
    
    commit that base (with drivers & stubs) so your intent is clear.
    
    Then add in the implementation details & test.

--- java/quadrant/src/main/java/quadrant/Quadrant.java
+++ java/quadrant/src/main/java/quadrant/Quadrant.java
@@ -4,19 +4,20 @@ import static common.MathUtils.clamp;
 
 /**
  * The Quadrant class consists of the following operations:
- * - Get klingons (gets the number of klingons in a quadrant)
- * - Get bases (gets the number of bases in a quadrant)
- * - Get stars (gets the number of stars in a quadrant)
- * - Set a new raw kbs value (with clamping)
- * - Get the raw kbs value
- * - Set a new value for each type (set a new klingon number, base number, or stars number)
+ * - Get klingons (gets the number of klingons in a quadrant)  √
+ * - Get bases (gets the number of bases in a quadrant)        √
+ * - Get stars (gets the number of stars in a quadrant)        √
+ * - Set a new raw kbs value (with clamping) ? public - API?
+ * - Get the raw kbs value ? public API?
+ * - Set a new value for each type (set a new klingon number, base number, or stars number) √
  * 
  * The Quadrant class can be constructed from:
- * - Nothing, will use a RNG to make a new random quadrant
- * - From an initial value, which content is set to
- * - From klingons, bases, and stars, with the correct clamping
+ * - Nothing, will use a RNG to make a new random quadrant √
+ * - From an initial value, which content is set to        √
+ * - From klingons, bases, and stars, with the correct clamping  √
  * 
  * There can be between 0-3 klingons per quadrant, 0-1 bases per quadrant, and 1-9 stars per quadrant [1 -> 319]
+ * NB: Stars cap out at 8. [0..318]
  * 
  */
 
@@ -26,6 +27,9 @@ public class Quadrant {
     
     /**
      * Generates a quadrant using a RNG
+
+         -> there are specific rules for creating quadrants
+             we will look at them in future sessions
      */
     public Quadrant() {
         Random rand = new Random();
@@ -38,15 +42,6 @@ public class Quadrant {
     }
 
     /**
-     * Constructs the Quadrant based off a raw KBS value
-     * 
-     * @param initValue
-     */
-    public Quadrant(int initValue) {
-        kbs = clampKBS(initValue);
-    }
-
-    /**
      * Constructs the Quadrant based off the number of klingons, bases, and stars wanted
      * 
      * @param klingons
@@ -54,7 +49,19 @@ public class Quadrant {
      * @param stars
      */
     public Quadrant(int klingons, int bases, int stars) {
-        kbs = clampKBS(klingons * 100 + bases * 10 + stars);
+        kbs = clampKBS(klingons * 100 + bases * 10 + stars);    // correct formula for encoding contents
+    }
+    
+    /**
+     * Constructs the Quadrant based off a raw KBS value
+
+         When you expose the contents value as part of the API you break encapsulation!
+         
+     * 
+     * @param initValue
+     */
+    private Quadrant(int initValue) {
+        kbs = clampKBS(initValue);
     }
 
     /**
@@ -82,32 +89,46 @@ public class Quadrant {
     }
 
     /**
-     * 
+     * You should NEVER require access to the encoded value.
+     
      * @return Quadrant's raw KBS value
      */
-    int raw() {
+    private int _raw() {
         return kbs;
     }
 
     /**
      * Sets the raw KBS value to a new value (clamps it)
+
+         This should bot be part of the public API
+         
      * 
      * @param newValue
      */
-    void setContent(int newValue) {
+    private void setContent(int newValue) {
         kbs = clampKBS(newValue);
     }
 
     /**
+     *    Mutator (set) Methods need to ensure that this SHOULD be part
+         of the public? API
+
+          Further, whenever you see the same formula or piece of code used
+          (repeated) in code, you need to factor that code so there is ONLY
+          one place for it to be maintains and tested!
+     */
+    
+    /**
      * Sets the klingon to a new value, and clamps it.
-     * It ensures none of the other values are changed
+     * It ensures none of the other values are changed √
+         !! This last point is very important! -- Well done!
      * 
      * @param newValue
      */
     void setKlingons(int newValue) {
         newValue = clamp(newValue, KLINGON_MIN, KLINGON_MAX);
         
-        kbs = newValue * 100 + bases() * 10 + stars();
+        kbs = newValue * 100 + bases() * 10 + stars();    // second copy of encoding formula
     }
 
     /**
@@ -119,7 +140,7 @@ public class Quadrant {
     void setBases(int newValue) {
         newValue = clamp(newValue, BASE_MIN, BASE_MAX);
         
-        kbs = klingons() * 100 + newValue * 10 + stars();
+        kbs = klingons() * 100 + newValue * 10 + stars();    // third copy of encoding formula
     }
 
     /**
@@ -131,14 +152,17 @@ public class Quadrant {
     void setStars(int newValue) {
         newValue = clamp(newValue, STAR_MIN, STAR_MAX);
         
-        kbs = klingons() * 100 + bases() * 10 + newValue;
+        kbs = klingons() * 100 + bases() * 10 + newValue;    // fourth copy of encoding formula
     }
 
     /**
      * Tests the clampKBS function to ensure it clamps the value correctly
+
+         -> other methods remain un-tested!
+         
      */
     void whiteBoxTest() {
-        int value = clampKBS(319);
+        int value = clampKBS(319);    // 319 incorrect --> 318
         assert value == 319  : "Value did not clamp correctly";
     
         value = clampKBS(500);
@@ -160,7 +184,13 @@ public class Quadrant {
     }
 
     /**
-     * 
+     * This is a good example where clamping may not be the correct choice.
+         -> we would prefer not to use the KBS values directly (via parameters)
+         -> if the user (programmers) attempt to set an invalid KBS value, they
+             should be notified that it is invalid!; as this is an error in programming!
+         -> silent correction of values need to be done with careful thought. 
+             It can lead to very difficult to trace/debug.
+             
      * @param kbs
      * @return kbs formatted to ensure that none of the values exceed its limit
      */
@@ -176,16 +206,17 @@ public class Quadrant {
         // for example: 581 becomes 311
         // another example: 000 becomes 001
 
-        return k * 100 + b * 10 + s;
+        return k * 100 + b * 10 + s;        // fifth encoding formula
     }
 
     // Data
-    private int kbs;
+    private int kbs;        // this could (eventually) be a char type saving 50% storage
+                            // but it is correct that it is not so now! but should be commented
     
     // Constants
     static final int KLINGON_MAX = 3;
     static final int BASE_MAX = 1;
-    static final int STAR_MAX = 9;
+    static final int STAR_MAX = 9;        // 8!
     static final int KLINGON_MIN = 0;
     static final int BASE_MIN = 0;
     static final int STAR_MIN = 1;
