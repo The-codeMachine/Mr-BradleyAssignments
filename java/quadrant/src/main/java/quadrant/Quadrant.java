9caa742da7e749556f31fc7b80b0dfcb3b0107e6
The-codeMachine <coolestbro5113@gmail.com>
2026-04-10 18:02:23 -0600

    starting fixes

--- java/quadrant/src/main/java/quadrant/Quadrant.java
+++ java/quadrant/src/main/java/quadrant/Quadrant.java
@@ -7,8 +7,6 @@ import static common.MathUtils.clamp;
  * - Get klingons (gets the number of klingons in a quadrant)  √
  * - Get bases (gets the number of bases in a quadrant)        √
  * - Get stars (gets the number of stars in a quadrant)        √
- * - Set a new raw kbs value (with clamping) ? public - API?
- * - Get the raw kbs value ? public API?
  * - Set a new value for each type (set a new klingon number, base number, or stars number) √
  * 
  * The Quadrant class can be constructed from:
@@ -38,7 +36,7 @@ public class Quadrant {
         int b = rand.nextInt(BASE_MIN, BASE_MAX);
         int s = rand.nextInt(STAR_MIN, STAR_MAX);
 
-        kbs = k * 100 + b * 10 + s;
+        setContent(k, b, s);
     }
 
     /**
@@ -49,19 +47,7 @@ public class Quadrant {
      * @param stars
      */
     public Quadrant(int klingons, int bases, int stars) {
-        kbs = clampKBS(klingons * 100 + bases * 10 + stars);    // correct formula for encoding contents
-    }
-    
-    /**
-     * Constructs the Quadrant based off a raw KBS value
-
-         When you expose the contents value as part of the API you break encapsulation!
-         
-     * 
-     * @param initValue
-     */
-    private Quadrant(int initValue) {
-        kbs = clampKBS(initValue);
+        setContent(klingons, bases, stars);
     }
 
     /**
@@ -87,136 +73,134 @@ public class Quadrant {
     public int stars() {
         return kbs % 10;
     }
-
-    /**
-     * You should NEVER require access to the encoded value.
-     
-     * @return Quadrant's raw KBS value
-     */
-    private int _raw() {
-        return kbs;
-    }
-
-    /**
-     * Sets the raw KBS value to a new value (clamps it)
-
-         This should bot be part of the public API
-         
-     * 
-     * @param newValue
-     */
-    private void setContent(int newValue) {
-        kbs = clampKBS(newValue);
-    }
-
-    /**
-     *    Mutator (set) Methods need to ensure that this SHOULD be part
-         of the public? API
-
-          Further, whenever you see the same formula or piece of code used
-          (repeated) in code, you need to factor that code so there is ONLY
-          one place for it to be maintains and tested!
-     */
     
     /**
-     * Sets the klingon to a new value, and clamps it.
-     * It ensures none of the other values are changed √
-         !! This last point is very important! -- Well done!
-     * 
-     * @param newValue
-     */
-    void setKlingons(int newValue) {
-        newValue = clamp(newValue, KLINGON_MIN, KLINGON_MAX);
-        
-        kbs = newValue * 100 + bases() * 10 + stars();    // second copy of encoding formula
+     *    Mutator (set) Methods need to ensure that this SHOULD be part
+    of the public? API
+    
+    Further, whenever you see the same formula or piece of code used
+    (repeated) in code, you need to factor that code so there is ONLY
+    one place for it to be maintains and tested!
+    */
+   
+   /**
+    * Sets the klingon to a new value, and clamps it.
+    * It ensures none of the other values are changed √
+   !! This last point is very important! -- Well done!
+   * 
+   * @param newValue
+   */
+  void setKlingons(int newValue) {
+      setContent(newValue, bases(), stars());
     }
-
+    
     /**
      * Sets the base to a new value, and clamps it.
      * It ensures none of the other values are changed
      * 
      * @param newValue
-     */
-    void setBases(int newValue) {
-        newValue = clamp(newValue, BASE_MIN, BASE_MAX);
-        
-        kbs = klingons() * 100 + newValue * 10 + stars();    // third copy of encoding formula
+    */
+   void setBases(int newValue) {
+       setContent(klingons(), newValue, stars());
     }
-
+    
     /**
      * Sets the stars to a new value, and clamps it.
      * It ensures none of the other values are changed
      * 
      * @param newValue
-     */
-    void setStars(int newValue) {
-        newValue = clamp(newValue, STAR_MIN, STAR_MAX);
-        
-        kbs = klingons() * 100 + bases() * 10 + newValue;    // fourth copy of encoding formula
+    */
+   void setStars(int newValue) {
+       setContent(klingons(), bases(), newValue);
     }
-
+    
     /**
      * Tests the clampKBS function to ensure it clamps the value correctly
-
-         -> other methods remain un-tested!
-         
-     */
-    void whiteBoxTest() {
-        int value = clampKBS(319);    // 319 incorrect --> 318
-        assert value == 319  : "Value did not clamp correctly";
     
-        value = clampKBS(500);
-        assert value == 301 : "Value did not clamp correctly";
-
-        value = clampKBS(257);
-        assert value == 217 : "Value did not clamp correctly";
-
-        value = clampKBS(233);
-        assert value == 213  : "Value did not clamp correctly";
+    -> other methods remain un-tested!
+    
+    */
+   void whiteBoxTest() {
+       int value = clampKBS(318);    // 319 incorrect --> 318
+       assert value == 318  : "Value did not clamp correctly";
+       
+       value = clampKBS(500);
+       assert value == 301 : "Value did not clamp correctly";
+       
+       value = clampKBS(257);
+       assert value == 217 : "Value did not clamp correctly";
+       
+       value = clampKBS(233);
+       assert value == 213  : "Value did not clamp correctly";
     }
-
+    
     /**
      * Returns a string value constructed from the number of klingons, bases, and stars
-     */
-    @Override
-    public String toString() {
-        return String.format("Klingons: %d, Bases: %d, Stars: %d", klingons(), bases(), stars());
+    */
+   @Override
+   public String toString() {
+       return String.format("Klingons: %d, Bases: %d, Stars: %d", klingons(), bases(), stars());
     }
+    
+    /**
+     * Sets the raw KBS value to a new value (clamps it)
 
+        This should bot be part of the public API
+            
+        * 
+        * @param newValue
+        */
+    private void setContent(int klingons, int bases, int stars) {
+        if (klingons > KLINGON_MAX || klingons < KLINGON_MIN)
+            throw new RuntimeException("Klingon exceed MAX, or dropped under MIN");
+
+        if (klingons > KLINGON_MAX || klingons < KLINGON_MIN)
+            throw new RuntimeException("Klingon exceed MAX, or dropped under MIN");
+
+        if (klingons > KLINGON_MAX || klingons < KLINGON_MIN)
+            throw new RuntimeException("Klingon exceed MAX, or dropped under MIN");
+
+        klingons = clamp(klingons, KLINGON_MIN, KLINGON_MAX);
+        bases = clamp(bases, BASE_MIN, BASE_MAX);
+        stars = clamp(stars, STAR_MIN, STAR_MAX);
+
+        kbs = klingons * 100 + bases * 10 + stars;
+    }
+    
     /**
      * This is a good example where clamping may not be the correct choice.
-         -> we would prefer not to use the KBS values directly (via parameters)
-         -> if the user (programmers) attempt to set an invalid KBS value, they
-             should be notified that it is invalid!; as this is an error in programming!
-         -> silent correction of values need to be done with careful thought. 
-             It can lead to very difficult to trace/debug.
-             
-     * @param kbs
-     * @return kbs formatted to ensure that none of the values exceed its limit
-     */
-    private int clampKBS(int kbs) {
-        int k = kbs / 100; // gets the number of klingons
-        int b = (kbs / 10) % 10; // gets the number of bases
-        int s = kbs % 10; // gets the number of stars
-
-        k = clamp(k, KLINGON_MIN, KLINGON_MAX);
-        b = clamp(b, BASE_MIN, BASE_MAX);
-        s = clamp(s, STAR_MIN, STAR_MAX);
-
-        // for example: 581 becomes 311
-        // another example: 000 becomes 001
-
-        return k * 100 + b * 10 + s;        // fifth encoding formula
+    -> we would prefer not to use the KBS values directly (via parameters)
+    -> if the user (programmers) attempt to set an invalid KBS value, they
+    should be notified that it is invalid!; as this is an error in programming!
+    -> silent correction of values need to be done with careful thought. 
+    It can lead to very difficult to trace/debug.
+    
+    * @param kbs
+    * @return kbs formatted to ensure that none of the values exceed its limit
+    */
+   private int clampKBS(int kbs) {
+       int k = kbs / 100; // gets the number of klingons
+       int b = (kbs / 10) % 10; // gets the number of bases
+       int s = kbs % 10; // gets the number of stars
+       
+       k = clamp(k, KLINGON_MIN, KLINGON_MAX);
+       b = clamp(b, BASE_MIN, BASE_MAX);
+       s = clamp(s, STAR_MIN, STAR_MAX);
+       
+       // for example: 581 becomes 311
+       // another example: 000 becomes 001
+       
+       return k * 100 + b * 10 + s;        // fifth encoding formula
     }
-
+    
     // Data
     private int kbs;        // this could (eventually) be a char type saving 50% storage
-                            // but it is correct that it is not so now! but should be commented
+    // but it is correct that it is not so now! but should be commented
     
     // Constants
     static final int KLINGON_MAX = 3;
     static final int BASE_MAX = 1;
-    static final int STAR_MAX = 9;        // 8!
+    static final int STAR_MAX = 8;
     static final int KLINGON_MIN = 0;
     static final int BASE_MIN = 0;
     static final int STAR_MIN = 1;
