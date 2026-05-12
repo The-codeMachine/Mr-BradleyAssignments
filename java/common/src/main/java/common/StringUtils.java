6bf980acd899fa0cd0a784fdbbf1f78cb2013869
The-codeMachine <coolestbro5113@gmail.com>
2026-05-11 18:45:20 -0600

    Fixed the Java padding (adding width)

--- java/common/src/main/java/common/StringUtils.java
+++ java/common/src/main/java/common/StringUtils.java
@@ -19,34 +19,50 @@ public class StringUtils {
      * Pads a message to the left
      * 
      * @param message
+     * @param width the width of the message plus padding
      * @return a string with the message padded to the left
      */
-    public static String padLeft(String message) {
-        return "\t" + message;
+    public static String padLeft(String message, int width) {
+        assert width > message.length();
+
+        String spaces = " ".repeat(width - message.length());
+
+        return spaces + message;
     }
 
-    
     /**
      * 
      * Pads a message to the right
      * 
      * @param message
+     * @param width the width of the message plus padding
      * @return a string with the message padded to the right
      */
-    public static String padRight(String message) {
-        return message + "\t";
+    public static String padRight(String message, int width) {
+        assert width > message.length();
+
+        String spaces = " ".repeat(width - message.length());
+
+        return message + spaces;
     }
 
-    
     /**
      * 
      * Pads a message to the center
      * 
+     * @apiNote if the width ends with a odd number (after the message), the extra
+     *          space not be accounted for
+     * 
      * @param message
-     * @return a string with the message padded to the center 
+     * @param width the width of the message plus padding
+     * @return a string with the message padded to the center
      */
-    public static String padCenter(String message) {
-        return "\t" + message + "\t";
+    public static String padCenter(String message, int width) {
+        assert width > message.length();
+
+        String spaces = " ".repeat((width - message.length()) / 2);
+
+        return spaces + message + spaces;
     }
 
     /**
@@ -54,29 +70,30 @@ public class StringUtils {
      * Zero fills a message with size many zeros
      * 
      * @param in
-     * @param size
+     * @param width
      * @return the message zero filled (size many zeros)
      */
-    public static String zeroFill(String in, int size) {
-        if (in.length() >= size)
+    public static String zeroFill(String in, int width) {
+        if (in.length() >= width)
             return in;
 
-        return "0".repeat(size - in.length()) + in;
+        return "0".repeat(width - in.length()) + in;
     }
 
     public static void stringUtilsTestDriver() {
         System.out.println("String utils test");
 
-        String msgLeftPadded = padLeft("something cool");
-        assert msgLeftPadded.equals("\tsomething cool") : "Message was not padded correctly";
+        // 14 characters is "something cool"
+        String msgLeftPadded = padLeft("something cool", 18);
+        assert msgLeftPadded.equals("    something cool") : "Message was not padded correctly";
         System.out.printf("Message padded left: %s\n", msgLeftPadded);
 
-        String msgRightPadded = padRight("something cool");
-        assert msgRightPadded.equals("something cool\t") : "Message was not padded correctly";
+        String msgRightPadded = padRight("something cool", 20);
+        assert msgRightPadded.equals("something cool      ") : "Message was not padded correctly";
         System.out.printf("Message padded right: %s\n", msgRightPadded);
 
-        String msgCenterPadded = padCenter("something cool");
-        assert msgCenterPadded.equals("\tsomething cool\t") : "Message was not padded correctly";
+        String msgCenterPadded = padCenter("something cool", 21);
+        assert msgCenterPadded.equals("   something cool   ") : "Message was not padded correctly";
         System.out.printf("Message padded center: %s\n", msgCenterPadded);
 
         String zeroFillStr = zeroFill("189", 4);
@@ -93,9 +110,9 @@ public class StringUtils {
  * Sample Output
  * 
  * String utils test
- * Message padded left: something cool
- * Message padded right: something cool
- * Message padded center: something cool
+ * Message padded left:     something cool
+ * Message padded right: something cool      
+ * Message padded center:    something cool   
  * Zero filled (str): 0189
  * String utils test success
  * 
