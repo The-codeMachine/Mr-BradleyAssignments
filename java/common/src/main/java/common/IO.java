95e729428e01e1863053dc4a090fa8e7ce9f1f19
The-codeMachine <coolestbro5113@gmail.com>
2026-07-17 11:42:16 -0600

    Made a basic start to the IO and logging system (Java)

---
+++ java/common/src/main/java/common/IO.java
@@ -0,0 +1,90 @@
+package common;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+/**
+ * 
+ * This is the input/output library. It allows
+ * you to prompt the user for input, recieve the
+ * input, and output lines. Current list of 
+ * operations include:
+ *  - Print a String to the console
+ *  - Print a line to the console
+ *  - Prompt the user for input
+ *  - Read a String/int/double from the user
+ *  - Set a log level
+ *  - Log a trace/warning/error
+ *  - Trace the stack (prints the stack to the exception)
+ * 
+ */
+public class IO {
+    
+    /**
+     * 
+     * Prints a message to the console. This message
+     * will not be formatted. 
+     * 
+     * @param msg
+     */
+    public static void print(String msg) {
+        System.out.print(msg);
+    }
+
+    /**
+     * 
+     * Prints a message as a line to the console. This will
+     * produce the message and a new line. 
+     * 
+     * @param msg
+     */
+    public static void println(String msg) {
+        System.out.println(msg);
+    }
+
+    /**
+     * 
+     * Prompts the user with a message. The scanner will
+     * then read the user's response to the message as a String. 
+     * 
+     * @param msg
+     * @return the user's response to the message as a String.
+     */
+    public static String prompt(String msg) {
+        print(msg);
+        return SCANNER.nextLine();
+    }
+
+    /**
+     * 
+     * Reads the next line from the user.
+     * 
+     * @return the next line from the user.
+     */
+    public static String readString() {
+        return SCANNER.nextLine();
+    }
+
+    /**
+     * 
+     * Reads the next integer from the user.
+     * 
+     * @return the next integer from the user.
+     */
+    public static int readInt() {
+        return SCANNER.nextInt();
+    }
+    
+    /**
+     * 
+     * Reads the next double from the user.
+     * 
+     * @return the next double from the user. 
+     */
+    public static double readDouble() {
+        return SCANNER.nextDouble();
+    }
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+}
---
+++ java/common/src/main/java/common/Logger.java
@@ -0,0 +1,117 @@
+package common;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+enum LogLevel {
+    Trace,
+    Warning,
+    Error
+}
+
+/**
+ * 
+ * The logger class logs messages to the console
+ * and to a file. This class allows you to change
+ * its log level, and log to certain levels. It
+ * uses an enum to represent its levels, this
+ * consists of:
+ *  - Trace
+ *  - Warning
+ *  - Error
+ * 
+ * The logger also has to ability to print stack
+ * traces. It is used within the IO library, and
+ * should only really be access through the IO 
+ * library. 
+ * 
+ * Currently, the logger only logs to the console. 
+ * There is no file logging yet. 
+ * 
+ */
+public class Logger {
+
+    Logger(LogLevel level) {
+        this.level = level;
+    }
+    
+    /**
+     * 
+     * Gets the log level for this logger. 
+     * 
+     * @return this logger's log level.
+     */
+    public LogLevel getLogLevel() {
+        return level;        
+    } 
+
+    /**
+     * 
+     * Sets the log level of this logger. 
+     * 
+     * @param level
+     */
+    public void setLogLevel(LogLevel level) {
+        this.level = level;
+    }
+
+    /**
+     * 
+     * Logs a message to the console (no file logging yet). 
+     * Checks that the log level supports logging that type. 
+     * 
+     * @param level
+     * @param msg
+     */
+    public void log(LogLevel level, String msg) {
+        if (this.level.ordinal() < level.ordinal()) {
+            // (e.g. warning logs does not log trace logs)
+            return; 
+        }
+
+        logMessage(msg);
+    }
+
+    /**
+     * 
+     * Prints the log message to the console. This
+     * log message is based off an exception stack
+     * trace. 
+     * 
+     * @param e
+     */
+    public void exception(Exception e) {
+        List<String> stackTrace = traceStack(e);
+
+        stackTrace.forEach(System.out::println);
+    }
+
+    /**
+     * 
+     * Logs the message to the console (will add
+     * file logging, but later).
+     * 
+     * @param msg
+     */
+    private void logMessage(String msg) {
+        System.out.println(msg);
+    }
+
+    /**
+     * 
+     * Constructs a list of strings representing
+     * the stack trace. 
+     * 
+     * @return a list of strings representing the stack trace. 
+     */
+    private static List<String> traceStack(Exception e) {
+        List<String> stackTraceList = Arrays.stream(e.getStackTrace())
+                    .map(StackTraceElement::toString)
+                    .collect(Collectors.toList());
+
+        return stackTraceList;
+    }
+
+    private LogLevel level; 
+}
